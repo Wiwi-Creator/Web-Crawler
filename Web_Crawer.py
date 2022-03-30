2d272a0a0a182de4ea418a885e05b653cb68fe32
Wiwi_Kuo <w22151500@kimo.com>
2022-03-30 21:10:23 +0800

    Update Web_Crawer.py

--- Web_Crawer.py
+++ Web_Crawer.py
@@ -5,3 +5,4 @@ a = requests.get("https://www.ptt.cc/bbs/MobileComm/index.html") #將此網頁HT
 print(a.text) #將網頁HTML印出
 soup   = BeautifulSoup(a.text,"html.parser") #將網頁資料存入html.parser
 sel = soup.select("div.title a")#取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
+
