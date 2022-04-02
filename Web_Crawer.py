f90d7fd4e7d25d7be93b5acb5eb79a066d254d76
Wiwi_Kuo <w22151500@kimo.com>
2022-04-02 22:25:28 +0800

    Delete Web_Crawer.py

--- Web_Crawer.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-a = requests.get("https://www.ptt.cc/bbs/MobileComm/index.html") #將此網頁HTML 利用get()取下
-print(a.text) #將網頁HTML印出
-soup   = BeautifulSoup(a.text,"html.parser") #將網頁資料存入html.parser
-sel = soup.select("div.title a")#取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
-
